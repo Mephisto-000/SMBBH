e7d173ae14a708a8730b81771a9b75d853b319d1
Mephisto <jackorgordon@gmail.com>
2023-02-16 16:29:42 +0800

    feat: add constant c option.

--- my_module/main.py
+++ my_module/main.py
@@ -4,8 +4,8 @@ from smbbh_nu import SMBBH_NU
 from plot_process import Plot_Result
 
 
-def potential_1(c, tmp_array, ratio_mass, r):
-    ratio_term = c*(tmp_array*ratio_mass/r)
+def potential_1(constant_c, tmp_array, ratio_mass, r):
+    ratio_term = constant_c*(tmp_array*ratio_mass/r)
     first_term = 1/(r + r**3)
     second_term = np.arctan(r)/(r**2)
     V = ratio_term*(first_term - second_term)
@@ -13,9 +13,11 @@ def potential_1(c, tmp_array, ratio_mass, r):
 
 
 if __name__ == "__main__":
-    black_hole_mass = [0.5, 0.5]  # m1, m2
+    total_smbbh_mass = 1.2*(10**12)
+    black_hole_mass = [total_smbbh_mass/2, total_smbbh_mass/2]  # m1, m2
     angles = [np.pi/6, np.pi/4, np.pi/6]  # omega, I, Omega
-    experiment_1 = SMBBH_NU(black_hole_mass, radius=0.2, eccentricity=0.5, angles=angles,
+    constant_c = 1000
+    experiment_1 = SMBBH_NU(black_hole_mass, constant_c, radius=0.2, eccentricity=0, angles=angles,
                             potential_function=potential_1)
     result_1 = experiment_1.all_result_output()
 
