fix: update correct numerical process.

src/main.py
@@ -4,44 +4,69 @@ from smbbh_nu import SMBBH_NU
 from plot_process import Plot_Result
 
 
-def potential_1(constant_c, tmp_array, ratio_mass, r):
-    ratio_term = constant_c*(tmp_array*ratio_mass/r)
+def potential_1(constant_c, comp_vector, r):
+    c_term = (constant_c*comp_vector) / r
     first_term = 1/(r + r**3)
     second_term = np.arctan(r)/(r**2)
-    V = ratio_term*(first_term - second_term)
+    V = c_term*(first_term - second_term)
     return V
 
 
 if __name__ == "__main__":
     black_hole_mass = [0.5, 0.5]  # m1(0.5), m2(0.5)
-    radius = 0.5
-    eccentricity = 0
+    t_0 = 0
+    t_f = 50
+    radius = 2  # 2
+    eccentricity = 0.7  # 0.7
     angles = [np.pi/6, np.pi/4, np.pi/6]  # omega, I, Omega
-    constant_c = 0  # 1000
+    # angles = [0.0, 0.0, 0.0]  # omega, I, Omega
+    constant_c = 0.9  # 1.0, 0.99, 0.9, 0.8
+
     experiment_1 = SMBBH_NU(black_hole_mass,
+                            t0=t_0,
+                            tf=t_f,
                             constant_c=constant_c,
                             radius=radius,
                             eccentricity=eccentricity,
                             angles=angles,
                             potential_function=potential_1)
-    result_1 = experiment_1.run()
+
+    exp_dict = experiment_1.run()
 
     while True:
+        os.system("clear")
+        print()
+        print("To display the trajectory chart for the supermassive binary black hole orbit, please press 'c1'")
+        print("To display the total energy change chart for the supermassive binary black holes, please press 'c2'")
+        print("To display the total energy change divided by the initial total energy chart for the "
+              "supermassive binary black holes, please press 'c3'")
+        print("To display the video of the trajectory chart for the supermassive binary black hole "
+              "orbit, please press 'c4'")
+        print("To save the video of the trajectory chart for the supermassive binary black hole "
+              "orbit, please press 'c5'")
+        print("To exit, please press 'q'")
+        print()
+        print()
+
         test_command = str(input("Please input the test case: "))
-        plot1 = Plot_Result(result_dict=result_1)
+        if test_command == "q":
+            break
+
+        rot_command = str(input("Would you like to rotate the binary black hole orbit trajectory in 3D? [y]/n : "))
+        rot_mod = "rotation" if rot_command == "y" else "no_rotation"
+        plot = Plot_Result(exp_dict, radius, t_0, t_f)
+
         if test_command == "c1":
-            plot1.plot_rk4_result(radius=radius)
-            break
+            plot.plot_rk4_result(mode=rot_mod)
         elif test_command == "c2":
-            plot1.plot_total_energy()
-            break
+            plot.plot_total_energy(mode=rot_mod)
         elif test_command == "c3":
-            plot1.plot_total_energy_divid_initE()
-            break
+            plot.plot_total_energy_divid_initE(mode=rot_mod)
         elif test_command == "c4":
-            show_mode = input("Please choose show mode [plot/save] : ")
-            plot1.plot_orbit_video(radius=0.05, show_mode=show_mode)
-            break
+            plot.plot_orbit_video(mode=rot_mod)
+        elif test_command == "c5":
+            plot.plot_orbit_video(mode=rot_mod, show_mode="save")
         else:
-            os.system("clear")
             print("Input Error! Please input again.")
+
+

src/plot_process.py
@@ -5,95 +5,136 @@ from matplotlib.animation import FuncAnimation
 
 
 class Plot_Result:
-    def __init__(self, result_dict):
-        self.no_rot_data = result_dict['no_rot_data']
-        self.rot_data = result_dict['rot_data']
-        self.total_energy = result_dict['total_energy']
-        self.init_energy = result_dict['initial_energy']
-        self.time_length = result_dict['time_length']
-        self.dt = result_dict['dt']
+    def __init__(self, all_result, radius, t0, tf, tl=1000):
+        self.no_rot_data = all_result['no_rot_data']
+        self.rot_data = all_result['rot_data']
 
-    def plot_rk4_result(self, radius, mode="rotation_data"):
-        if mode == "rotation_data":
-            result_array = self.rot_data
+        self.no_rot_total_energy = all_result['no_rot_energy']
+        self.no_rot_init_energy = all_result['no_rot_energy0']
+        self.total_energy = all_result['rot_energy']
+        self.init_energy = all_result['rot_energy0']
+
+        self.radius = radius
+        self.t0 = t0
+        self.tf = tf
+        self.time_length = np.linspace(t0, tf, tl)
+
+    def plot_rk4_result(self, mode="rotation"):
+        if mode == "rotation":
+            data_dict = self.rot_data
+            r1 = data_dict['p1_orbit']
+            r2 = data_dict['p2_orbit']
+
+        elif mode == "no_rotation":
+            data_dict = self.no_rot_data
+            r1 = data_dict['p1_orbit']
+            r2 = data_dict['p2_orbit']
         else:
-            result_array = self.no_rot_data
+            print("Test mode")
+            data_dict = self.no_rot_data
+            r1 = data_dict['p1_orbit']
+            r2 = data_dict['p2_orbit']
 
-        ax_len = radius / 2
-        x1, y1, z1 = result_array[1:, 0], result_array[1:, 1], result_array[1:, 2]
-        x2, y2, z2 = result_array[1:, 3], result_array[1:, 4], result_array[1:, 5]
-        # fig = plt.figure(figsize=(30, 10))
+        ax_len = self.radius * 0.75
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
         ax.set_xlim3d(-ax_len, ax_len)
         ax.set_ylim3d(-ax_len, ax_len)
         ax.set_zlim3d(-ax_len, ax_len)
-        ax.plot3D(x1, y1, z1, "blue", label="M1")
-        ax.plot3D(x2, y2, z2, "red", label="M2")
-        ax.set_xlabel('X [kpc]', fontsize=10)
-        ax.set_ylabel('Y [kpc]', fontsize=10)
-        ax.set_zlabel('Z [kpc]', fontsize=10)
-        ax.legend(loc="upper left", fontsize=15)
+        ax.plot(r1[:, 0], r1[:, 1], r1[:, 2], color='darkblue', alpha=0.4,  label="p1 orbit")
+        ax.plot(r2[:, 0], r2[:, 1], r2[:, 2], color='red', alpha=0.4, label="p2 orbit")
+        ax.plot(r1[0, 0], r1[0, 1], r1[0, 2], "bo", label="p1 orbit (t=0)")
+        ax.plot(r2[0, 0], r2[0, 1], r2[0, 2], "ro", label="p2 orbit (t=0)")
+        ax.plot(0, 0, 0, 'ko', label="COG")
+        ax.legend()
         plt.show()
 
-    def plot_total_energy(self):
-        Et = self.total_energy
-        plt.style.use("ggplot")
-        dt_len = np.linspace(0, self.time_length, self.time_length - 1)
-        plt.figure(figsize=(10, 4))
-        plt.subplot()
-        plt.plot(dt_len, Et, "-", color="darkblue")
-        plt.title(r'$E_{total}$,' + r' ($Max(|E_{total}|)\approx$' + f'{round(max(np.abs(Et)), 4)})', fontsize=20)
-        plt.xlabel("Time ($4.3*10^{15}$ years)", fontsize=10)
-        plt.show()
-
-    def plot_total_energy_divid_initE(self):
-        Et = self.total_energy
-        E0 = self.init_energy
-        Et_E0 = Et / E0
-        plt.style.use("ggplot")
-        dt_len = np.linspace(0, self.time_length, self.time_length - 1)
-        plt.figure(figsize=(10, 4))
-        plt.subplot()
-        plt.plot(dt_len, Et_E0, "-", color="darkblue")
-        plt.title(r'$E_{total}/E_{0}$,' + rf' ($E_{0}\approx$ {round(E0, 4)})', fontsize=20)
-        plt.xlabel("Time ($4.3*10^{15}$ years)", fontsize=10)
-        plt.show()
-
-    def plot_orbit_video(self, radius, mode="rotation_data", show_mode='plot', title=None):
-        if mode == "rotation_data":
-            result_array = self.rot_data[1::50, :]
+    def plot_total_energy(self, mode="rotation"):
+        if mode == "rotation":
+            total_energy = self.total_energy
+        elif mode == "no_rotation":
+            total_energy = self.no_rot_total_energy
         else:
-            result_array = self.no_rot_data[1::50, :]
+            print("Test mode")
+            total_energy = self.no_rot_total_energy
+
+        plt.style.use("ggplot")
+        plt.figure(figsize=(10, 4))
+        plt.subplot()
+        plt.plot(self.time_length, total_energy, "-", color="darkblue")
+        plt.title(r'$E_{total}$,' + r' ($Max(|E_{total}|)\approx$' + f'{round(max(np.abs(total_energy)), 4)})', fontsize=20)
+        plt.xlabel("Time ($4.3*10^{15}$ years)", fontsize=10)
+        plt.show()
+
+    def plot_total_energy_divid_initE(self, mode="rotation"):
+        if mode == "rotation":
+            total_energy = self.total_energy
+            initial_energy = self.init_energy
+        elif mode == "no_rotation":
+            total_energy = self.no_rot_total_energy
+            initial_energy = self.no_rot_init_energy
+        else:
+            print("Test mode")
+            total_energy = self.no_rot_total_energy
+            initial_energy = self.no_rot_init_energy
+
+        Et_divid_E0 = total_energy/initial_energy
+        plt.style.use("ggplot")
+        plt.figure(figsize=(10, 4))
+        plt.subplot()
+        plt.plot(self.time_length, Et_divid_E0, "-", color="darkblue")
+        plt.title(r'$E_{total}/E_{0}$,' + rf' ($E_{0}\approx$ {round(initial_energy, 4)})', fontsize=20)
+        plt.xlabel("Time ($4.3*10^{15}$ years)", fontsize=10)
+        plt.show()
+
+    def plot_orbit_video(self, mode="rotation", show_mode='plot', title=None):
+        if mode == "rotation":
+            data_dict = self.rot_data
+            r1 = data_dict['p1_orbit'][::5, :]
+            r2 = data_dict['p2_orbit'][::5, :]
+        elif mode == "no_rotation":
+            data_dict = self.no_rot_data
+            r1 = data_dict['p1_orbit'][::5, :]
+            r2 = data_dict['p2_orbit'][::5, :]
+        else:
+            print("Test mode")
+            data_dict = self.no_rot_data
+            r1 = data_dict['p1_orbit']
+            r2 = data_dict['p2_orbit']
+
         fig = plt.figure(figsize=(8, 8))
         ax = fig.add_subplot(111, projection='3d')
+        ax_len = self.radius * 0.75
+        ax.set_xlim3d(-ax_len, ax_len)
+        ax.set_ylim3d(-ax_len, ax_len)
+        ax.set_zlim3d(-ax_len, ax_len)
 
-        header_1 = [ax.scatter(result_array[1:, 0][0], result_array[1:, 1][0], result_array[1:, 2][0],
+        header_1 = [ax.scatter(r1[:, 0][0], r1[:, 1][0], r1[:, 2][0],
                                color="darkblue", marker="o", s=85, label="M1")]
-        header_2 = [ax.scatter(result_array[1:, 3][0], result_array[1:, 4][0], result_array[1:, 5][0],
+        header_2 = [ax.scatter(r2[:, 0][0], r2[:, 1][0], r2[:, 2][0],
                                color="red", marker="o", s=85, label="M2")]
 
         ax.set_xlabel("X", fontsize=14)
         ax.set_ylabel("Y", fontsize=14)
         ax.set_zlabel("Z", fontsize=14)
-        ax.set_title("Visualization Orbits of 2-body system\n", fontsize=16)
+        ax.set_title("Animation of the orbit trajectory for the SMBBH\n", fontsize=16)
         ax.legend(loc="upper left", fontsize=14)
 
         def ani(iter, M1, M2):
             header_1[0].remove()
             header_2[0].remove()
-            orb1 = ax.plot(result_array[1:, 0][:iter], result_array[1:, 1][:iter], result_array[1:, 2][:iter],
-                           color="darkblue")
-            orb2 = ax.plot(result_array[1:, 3][:iter], result_array[1:, 4][:iter], result_array[1:, 5][:iter],
-                           color="red")
-            header_1[0] = ax.scatter(result_array[1:, 0][iter], result_array[1:, 1][iter], result_array[1:, 2][iter],
+            orb1 = ax.plot(r1[:, 0][:iter], r1[:, 1][:iter], r1[:, 2][:iter],
+                           color="darkblue", alpha=0.3)
+            orb2 = ax.plot(r2[:, 0][:iter], r2[:, 1][:iter], r2[:, 2][:iter],
+                           color="red", alpha=0.3)
+            header_1[0] = ax.scatter(r1[:, 0][iter], r1[:, 1][iter], r1[:, 2][iter],
                                      color="darkblue", marker="o", s=85)
-            header_2[0] = ax.scatter(result_array[1:, 3][iter], result_array[1:, 4][iter], result_array[1:, 5][iter],
+            header_2[0] = ax.scatter(r2[:, 0][iter], r2[:, 1][iter], r2[:, 2][iter],
                                      color="red", marker="o", s=85)
 
             return orb1, orb2, header_1, header_2
 
-        frames = result_array[1:, 0].shape[0]
+        frames = r1[:, 0].shape[0]
         interval = 0.001
         anim_2b = FuncAnimation(fig, ani, frames=frames, interval=interval, repeat=False,
                                 blit=False, save_count=100,
@@ -103,10 +144,11 @@ class Plot_Result:
             plt.show()
         elif show_mode == 'save':
             ani_writer = animation.writers['ffmpeg']
-            writer = ani_writer(fps=10, metadata=dict(artist="Ling-Hao Lin"), bitrate=-1)
+            writer = ani_writer(fps=10, metadata=dict(artist="Lin-Hao Lin"), bitrate=-1)
             if title is None:
                 anim_2b.save(f"test.mp4", writer=writer, dpi=300)
             else:
                 anim_2b.save(f"{title}.mp4", writer=writer, dpi=300)
         else:
             return anim_2b
+

src/smbbh_nu.py
@@ -1,16 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class SMBBH_NU:
-    def __init__(self, black_holes_mass, time_length=5*10**3, dt=5*(10**(-4)), mass_ratio=None, **kwargs):
+    def __init__(self, black_holes, t0, tf, tl=1000, **kwargs):
         self.__G = 1
-        self.period = 10
-        self.__eq_amount = 6
-        self.time_length = time_length    # 1000
-        self.dt = dt                      # 0.0005
+        self.__eq_amount = 12
+        self.__tl = tl
+        self.t0 = t0
+        self.tf = tf
+        self.__dt = (tf - t0) / (tl - 1)
+        self.__t_p = np.linspace(t0, tf, tl)
 
-        self.bh_mass = [m_i for m_i in black_holes_mass]
-        self.mass_ratio = mass_ratio
+        self.bh_mass = {'m1': black_holes[0],
+                        'm2': black_holes[1]}
         self.c = kwargs['constant_c']
         self.r = kwargs['radius']
         self.e = kwargs['eccentricity']
@@ -18,117 +21,118 @@ class SMBBH_NU:
         self.rot_mat = np.identity(3)
         self.potential = kwargs['potential_function']
 
-        self.init_c_vel = np.sqrt(self.__G*self.bh_mass[1] / self.r)
+        self.init_c_vel = np.sqrt(self.__G*self.bh_mass['m1'] / self.r)
         self.init_e_vel = np.sqrt((1 - self.e)*(self.init_c_vel**2))
 
-        self.init_single_bh_array = np.array([self.r, 0.0, 0.0,
-                                              0.0, self.init_e_vel, 0.0])
-        self.result_array = np.ones((self.time_length, self.__eq_amount))
-        self.result_array_barycentric_sys = np.ones((self.time_length, self.__eq_amount*2))
+        self.r1_0 = np.array([self.r, 0.0, 0.0])
+        self.r2_0 = np.array([0.0, 0.0, 0.0])
+        self.v1_0 = np.array([0.0, self.init_e_vel, 0.0])
+        self.v2_0 = np.array([0.0, -self.init_e_vel, 0.0])
 
-        self.rotation_result = None
+        self.initial_value = np.hstack((self.r1_0, self.r2_0,
+                                        self.v1_0, self.v2_0))
+        self.y_noRot = np.ones((tl, self.__eq_amount))
+
+        self.noRot_result = None
+        self.rot_result = None
+        self.no_rot_energy = None
+        self.no_rot_energy_0 = None
         self.energy = None
         self.energy_0 = None
 
-    @staticmethod
-    def __two_body_system(eq_index, dt_step, tmp_array, mu):
-        r = np.sqrt(tmp_array[0]**2 + tmp_array[1]**2 + tmp_array[2]**2)
+    def __two_body_system(self, eq_index, tmp_array, mu):
 
+        xi = tmp_array[3] - tmp_array[0]
+        yi = tmp_array[4] - tmp_array[1]
+        zi = tmp_array[5] - tmp_array[2]
+
+        r = np.sqrt(xi**2 + yi**2 + zi**2)
+
+        # particle 1 orbit
         if eq_index == 0:
-            return tmp_array[3]
+            return tmp_array[6]
         elif eq_index == 1:
-            return tmp_array[4]
+            return tmp_array[7]
         elif eq_index == 2:
-            return tmp_array[5]
+            return tmp_array[8]
 
+        # particle 2 orbit
         elif eq_index == 3:
-            return -mu * tmp_array[0] / (r ** 3)
+            return tmp_array[9]
         elif eq_index == 4:
-            return -mu * tmp_array[1] / (r ** 3)
+            return tmp_array[10]
         elif eq_index == 5:
-            return -mu * tmp_array[2] / (r ** 3)
+            return tmp_array[11]
+
+        # particle 1 velocity
+        elif eq_index == 6:
+            return mu * xi / (r ** 3) + self.potential(self.c, xi, r)
+        elif eq_index == 7:
+            return mu * yi / (r ** 3) + self.potential(self.c, yi, r)
+        elif eq_index == 8:
+            return mu * zi / (r ** 3) + self.potential(self.c, zi, r)
+
+        # particle 2 velocity
+        elif eq_index == 9:
+            return -mu * xi / (r ** 3) - self.potential(self.c, xi, r)
+        elif eq_index == 10:
+            return -mu * yi / (r ** 3) - self.potential(self.c, yi, r)
+        elif eq_index == 11:
+            return -mu * zi / (r ** 3) - self.potential(self.c, zi, r)
+
         else:
             return 0
 
-    def __barycentric(self, eq_index, dt_step, tmp_array, bh_mass, constant_c, ratio_change):
-        if ratio_change is None:
-            ratio_change = [0, 0]
-        m1, m2 = bh_mass[0], bh_mass[1]
-        ratio_m1 = (m2 + ratio_change[0]) / (m1 + m2)
-        ratio_m2 = (m1 - ratio_change[1]) / (m1 + m2)
-
-        r1 = np.sqrt(tmp_array[0] ** 2 + tmp_array[1] ** 2 + tmp_array[2] ** 2) * ratio_m1
-        r2 = np.sqrt(tmp_array[0] ** 2 + tmp_array[1] ** 2 + tmp_array[2] ** 2) * ratio_m2
-
-        if eq_index == 0:
-            return tmp_array[0] * ratio_m1
-        elif eq_index == 1:
-            return tmp_array[1] * ratio_m1
-        elif eq_index == 2:
-            return tmp_array[2] * ratio_m1
-
-        elif eq_index == 3:
-            return -tmp_array[0] * ratio_m2
-        elif eq_index == 4:
-            return -tmp_array[1] * ratio_m2
-        elif eq_index == 5:
-            return -tmp_array[2] * ratio_m2
-
-        elif eq_index == 6:
-            return tmp_array[3] * ratio_m1 + self.potential(constant_c, tmp_array[0], ratio_m1, r1)
-        elif eq_index == 7:
-            return tmp_array[4] * ratio_m1 + self.potential(constant_c, tmp_array[1], ratio_m1, r1)
-        elif eq_index == 8:
-            return tmp_array[5] * ratio_m1 + self.potential(constant_c, tmp_array[2], ratio_m1, r1)
-
-        elif eq_index == 9:
-            return -tmp_array[3] * ratio_m2 + self.potential(constant_c, -tmp_array[0], ratio_m2, r2)
-        elif eq_index == 10:
-            return -tmp_array[4] * ratio_m2 + self.potential(constant_c, -tmp_array[1], ratio_m2, r2)
-        elif eq_index == 11:
-            return -tmp_array[5] * ratio_m2 + self.potential(constant_c, -tmp_array[2], ratio_m2, r2)
-
     def rk4_process(self):
-        m1, m2 = self.bh_mass
+        m1, m2 = self.bh_mass['m1'], self.bh_mass['m2']
         mu = self.__G*(m1 + m2)
 
         k1, k2 = np.ones(self.__eq_amount), np.ones(self.__eq_amount)
         k3, k4 = np.ones(self.__eq_amount), np.ones(self.__eq_amount)
         v1, v2, v3 = np.ones(self.__eq_amount), np.ones(self.__eq_amount), np.ones(self.__eq_amount)
         time_tmp = 1
-        dt_step = 0
 
         for index in range(self.__eq_amount):
-            self.result_array[0, index] = self.init_single_bh_array[index]
+            self.y_noRot[0, index] = self.initial_value[index]
 
-        while time_tmp < self.time_length:
+        while time_tmp < self.__tl:
             for index in range(self.__eq_amount):
-                k1[index] = self.dt*self.__two_body_system(index, dt_step, self.result_array[time_tmp - 1], mu)
-                v1[index] = self.result_array[time_tmp - 1][index] + 0.5*k1[index]
+                k1[index] = self.__dt*self.__two_body_system(index, self.y_noRot[time_tmp - 1], mu)
+                v1[index] = self.y_noRot[time_tmp - 1][index] + 0.5*k1[index]
             for index in range(self.__eq_amount):
-                k2[index] = self.dt*self.__two_body_system(index, dt_step + 0.5*self.dt, v1, mu)
-                v2[index] = self.result_array[time_tmp - 1][index] + 0.5*k2[index]
+                k2[index] = self.__dt*self.__two_body_system(index, v1, mu)
+                v2[index] = self.y_noRot[time_tmp - 1][index] + 0.5*k2[index]
             for index in range(self.__eq_amount):
-                k3[index] = self.dt*self.__two_body_system(index, dt_step + 0.5*self.dt, v2, mu)
-                v3[index] = self.result_array[time_tmp - 1][index] + k3[index]
+                k3[index] = self.__dt*self.__two_body_system(index, v2, mu)
+                v3[index] = self.y_noRot[time_tmp - 1][index] + k3[index]
             for index in range(self.__eq_amount):
-                k4[index] = self.dt*self.__two_body_system(index, dt_step + self.dt, v3, mu)
+                k4[index] = self.__dt*self.__two_body_system(index, v3, mu)
 
             for index in range(self.__eq_amount):
                 estimate_term = (k1[index] + 2*k2[index] + 2*k3[index] + k4[index]) / 6.0
-                self.result_array[time_tmp][index] = self.result_array[time_tmp - 1][index] + estimate_term
-
-            # Barycentric System :
-            for index in range(self.__eq_amount*2):
-                self.result_array_barycentric_sys[time_tmp][index] = self.__barycentric(index, dt_step,
-                                                                                        self.result_array[time_tmp],
-                                                                                        self.bh_mass,
-                                                                                        self.c,
-                                                                                        self.mass_ratio)
+                self.y_noRot[time_tmp][index] = self.y_noRot[time_tmp - 1][index] + estimate_term
 
             time_tmp += 1
-            dt_step += self.dt
-        print("rk4 process done !")
+        # print("rk4 process done !")
+
+    def no_rot_result(self):
+        m1, m2 = self.bh_mass['m1'], self.bh_mass['m2']
+        self.rk4_process()
+        r1, r2 = self.y_noRot[:, :3], self.y_noRot[:, 3:6]
+        v1, v2 = self.y_noRot[:, 6:9], self.y_noRot[:, 9:]
+
+        barycenter = (m1*r1 + m2*r2) / (m1 + m2)
+
+        r1_relation_com = r1 - barycenter
+        r2_relation_com = r2 - barycenter
+        v1_relation_com = v1 - barycenter
+        v2_relation_com = v2 - barycenter
+        no_rot_dict = {'p1_orbit': r1_relation_com,
+                       'p2_orbit': r2_relation_com,
+                       'p1_velocity': v1_relation_com,
+                       'p2_velocity': v2_relation_com}
+
+        self.noRot_result = no_rot_dict
 
     def rotation_mat(self):
         omega, I, Omega = self.angles
@@ -146,61 +150,100 @@ class SMBBH_NU:
         self.rot_mat = p3p2p1
 
     def rotation_data(self):
-        result_array = self.result_array_barycentric_sys
+        no_rot_dict = self.noRot_result
 
-        x1, y1, z1 = result_array[0:, 0], result_array[0:, 1], result_array[0:, 2]
-        x2, y2, z2 = result_array[0:, 3], result_array[0:, 4], result_array[0:, 5]
-
-        xv1, yv1, zv1 = result_array[0:, 6], result_array[0:, 7], result_array[0:, 8]
-        xv2, yv2, zv2 = result_array[0:, 9], result_array[0:, 10], result_array[0:, 11]
-
-        m1_orb = np.array([x1, y1, z1])
-        m2_orb = np.array([x2, y2, z2])
-        m1_vel = np.array([xv1, yv1, zv1])
-        m2_vel = np.array([xv2, yv2, zv2])
+        r1 = no_rot_dict['p1_orbit']
+        r2 = no_rot_dict['p2_orbit']
+        v1 = no_rot_dict['p1_velocity']
+        v2 = no_rot_dict['p2_velocity']
 
         self.rotation_mat()
-        ro1_data = np.dot(self.rot_mat, m1_orb).T
-        ro2_data = np.dot(self.rot_mat, m2_orb).T
-        ro1v_data = np.dot(self.rot_mat, m1_vel).T
-        ro2v_data = np.dot(self.rot_mat, m2_vel).T
+        rot_r1 = np.dot(self.rot_mat, r1.T).T
+        rot_r2 = np.dot(self.rot_mat, r2.T).T
+        rot_v1 = np.dot(self.rot_mat, v1.T).T
+        rot_v2 = np.dot(self.rot_mat, v2.T).T
 
-        ro_data = np.hstack([ro1_data, ro2_data, ro1v_data, ro2v_data])
-        self.rotation_result = ro_data
+        self.rot_result = {'p1_orbit': rot_r1,
+                           'p2_orbit': rot_r2,
+                           'p1_velocity': rot_v1,
+                           'p2_velocity': rot_v2}
+
 
     def cal_total_energy(self):
-        result_array = self.rotation_result
-        mass_sum = np.sum(self.bh_mass)
+        no_rot_data = self.noRot_result
+        rot_data = self.rot_result
+        mass_sum = self.bh_mass['m1'] + self.bh_mass['m2']
 
-        x1, y1, z1 = result_array[0:, 0], result_array[0:, 1], result_array[0:, 2]
-        x2, y2, z2 = result_array[0:, 3], result_array[0:, 4], result_array[0:, 5]
+        nx1, ny1, nz1 = no_rot_data['p1_orbit'][:, 0], no_rot_data['p1_orbit'][:, 1], no_rot_data['p1_orbit'][:, 2]
+        nx2, ny2, nz2 = no_rot_data['p2_orbit'][:, 0], no_rot_data['p2_orbit'][:, 1], no_rot_data['p2_orbit'][:, 2]
+        x1, y1, z1 = rot_data['p1_orbit'][:, 0], rot_data['p1_orbit'][:, 1], rot_data['p1_orbit'][:, 2]
+        x2, y2, z2 = rot_data['p2_orbit'][:, 0], rot_data['p2_orbit'][:, 1], rot_data['p2_orbit'][:, 2]
 
+        nr1 = np.sqrt(nx1**2 + ny1**2 + nz1**2)
+        nr2 = np.sqrt(nx2**2 + ny2**2 + nz2**2)
         r1 = np.sqrt(x1**2 + y1**2 + z1**2)
         r2 = np.sqrt(x2**2 + y2**2 + z2**2)
 
-        xv1, yv1, zv1 = result_array[0:, 6], result_array[0:, 7], result_array[0:, 8]
-        xv2, yv2, zv2 = result_array[0:, 9], result_array[0:, 10], result_array[0:, 11]
+        n_xv1 = no_rot_data['p1_velocity'][0:, 0]
+        n_yv1 = no_rot_data['p1_velocity'][0:, 1]
+        n_zv1 = no_rot_data['p1_velocity'][0:, 2]
+        n_xv2 = no_rot_data['p2_velocity'][0:, 0]
+        n_yv2 = no_rot_data['p2_velocity'][0:, 1]
+        n_zv2 = no_rot_data['p2_velocity'][0:, 2]
+
+        xv1 = rot_data['p1_velocity'][0:, 0]
+        yv1 = rot_data['p1_velocity'][0:, 1]
+        zv1 = rot_data['p1_velocity'][0:, 2]
+        xv2 = rot_data['p2_velocity'][0:, 0]
+        yv2 = rot_data['p2_velocity'][0:, 1]
+        zv2 = rot_data['p2_velocity'][0:, 2]
+
+        no_rotE1 = 0.5*(n_xv1**2 + n_yv1**2 + n_zv1**2) - (mass_sum/nr1) - self.c*(np.arctan(nr1)/nr1)
+        no_rotE2 = 0.5*(n_xv2**2 + n_yv2**2 + n_zv2**2) - (mass_sum/nr2) - self.c*(np.arctan(nr2)/nr2)
+        no_rot_total_E = no_rotE1 + no_rotE2
 
         E1 = 0.5*(xv1**2 + yv1**2 + zv1**2) - (mass_sum/r1) - self.c*(np.arctan(r1)/r1)
         E2 = 0.5*(xv2**2 + yv2**2 + zv2**2) - (mass_sum/r2) - self.c*(np.arctan(r2)/r2)
-        total_E = E1[1:] + E2[1:]
+        total_E = E1 + E2
 
+        self.no_rot_energy = no_rot_total_E
+        self.no_rot_energy_0 = no_rot_total_E[0]
         self.energy = total_E
         self.energy_0 = total_E[0]
 
-    # Output all result :
     def run(self):
-        print("Begin Simulation of Supermassive Binary Black Holes : ")
-        self.rk4_process()
+        self.no_rot_result()
         self.rotation_data()
         self.cal_total_energy()
-        print("All Processes Done !")
 
-        all_results = {'no_rot_data': self.result_array_barycentric_sys,
-                       'rot_data': self.rotation_result,
-                       'total_energy': self.energy,
-                       'initial_energy': self.energy_0,
-                       'time_length': self.time_length,
-                       'dt': self.dt}
+        all_result = {'no_rot_data': self.noRot_result,
+                      'rot_data': self.rot_result,
+                      'no_rot_energy': self.no_rot_energy,
+                      'no_rot_energy0': self.no_rot_energy_0,
+                      'rot_energy': self.energy,
+                      'rot_energy0': self.energy_0}
 
-        return all_results
+        return all_result
+
+    def test(self):
+        self.no_rot_result()
+        self.rotation_data()
+        self.cal_total_energy()
+
+        all_result = {'no_rot_data': self.noRot_result,
+                      'rot_data': self.rot_result,
+                      'no_rot_energy': self.no_rot_energy,
+                      'no_rot_energy0': self.no_rot_energy_0,
+                      'rot_energy': self.energy,
+                      'rot_energy0': self.energy_0}
+
+        return all_result
+
+
+
+
+
+
+
+
+